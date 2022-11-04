320dc8bfa64478f1554c6fa5d2792d338054849b
dthomas91 <davidthomas8891@gmail.com>
2022-11-04 16:24:40 -0700

    finished unprotected show

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -33,7 +33,7 @@ const App = (props) => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/apartmentindex" element={<ApartmentIndex />} />
-        <Route path="/apartmentshow" element={<ApartmentShow />} />
+        <Route path="/apartmentshow/:id" element={<ApartmentShow apartments={apartments} />} />
         <Route path="/apartmentnew" element={<ApartmentNew />} />
         <Route path="/apartmentedit" element={<ApartmentEdit />} />
         <Route element={<NotFound />} />
